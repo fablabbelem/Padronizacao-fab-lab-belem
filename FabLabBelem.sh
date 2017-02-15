305960f46dc4091ed9876821e87f7e3b28bd459e
moacirbmn <netinho_moacir@hotmail.com>
2017-02-15 16:03:45 -0300

    Add files via upload

--- FabLabBelem.sh
+++ FabLabBelem.sh
@@ -6,10 +6,11 @@ echo "==========================================================
         Digite o número do componente a ser instalado:
         1 - Instalar Pacotes do bCNC
         2 - Instalar Kicad
-        3 - Instalar FreeCAD
+        3 - Instalar Sublime Text
         4 - Instalar Inkscape
-	5 - Instalar LibreOffice
-	T - Instala todos os pacotes listados acima
+		5 - Instalar LibreOffice
+		6 - Instalar Repetier Host 1.6.2
+		T - Instala todos os pacotes listados acima
 
         0 - Sair
 ==========================================================="
@@ -51,25 +52,27 @@ echo "INSTALANDO KICAD"
 
     3)
 echo "ADICIONANDO REPOSITORIO AO LINUX"
- 	sudo add-apt-repository ppa:freecad-maintainers/freecad-daily -y
+	sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
-    	sudo apt-get update
+    sudo apt-get update
 	clear
 echo "INSTALANDO POSSIVEIS ATUALIZAÇÕES"
    	sudo apt-get upgrade -y
 	clear
-echo "INSTALANDO FREECAD"
-  	sudo apt-get install freecad-daily freecad-daily-doc -y;;
+echo "INSTALANDO SUBLIME TEXT"
+  	sudo apt-get install sublime-text-installer;;
+  	clear
 
 
     4)
 echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
-        sudo apt-get update
+    sudo apt-get update
 	clear
 echo "INSTALANDO POSSIVEIS ATUALIZAÇÕES"
 	sudo apt-get upgrade -y
 	clear
-	echo "INSTALANDO Inkscape"
+
+echo "INSTALANDO Inkscape"
 	sudo apt-get install inkscape -y;;      
 
     5)
@@ -81,29 +84,47 @@ echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
 echo "INSTALANDO LibreOffice"
 	sudo apt-get install libreoffice libreoffice-style-breeze;;
 	
+	6)
+echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
+    sudo apt-get update
+	clear
+echo "INSTALANDO POSSIVEIS ATUALIZAÇÕES"
+	sudo apt-get upgrade -y
+	clear
+
+echo "BAIXANDO Repetier Host"
+	wget -c http://download.repetier.com/files/host/linux/repetierHostLinux_1_6_2.tgz
+	clear
+
+echo "Descompactando Arquivo e Instalando"
+	tar -xzf repetierHostLinux_1_6_2.tgz
+	cd RepetierHost
+	bash configureFirst.sh
+	clear
+
 
     T)
-	echo "Iniciando Instalação de todos os pacotes"
+echo "Iniciando Instalação de todos os pacotes"
 
 #Verificando Atualizações do Sistema
-	echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
-        sudo apt-get update
+echo "VERIFICANDO SE HÁ ATUALIZAÇÕES"
+    sudo apt-get update
 	clear
-	echo "INSTALANDO POSSIVEIS ATUALIZAÇÕES"
+echo "INSTALANDO POSSIVEIS ATUALIZAÇÕES"
 	sudo apt-get upgrade -y
 	clear
 
 #Adicionando Repositorios
 	#Kicad
-	echo "ADICIONANDO REPOSITORIO AO LINUX-(KICAD)"
+echo "ADICIONANDO REPOSITORIO AO LINUX-(KICAD)"
         sudo add-apt-repository --yes ppa:js-reynaud/kicad-4 -y
 	
-	#Freecad
-	echo "ADICIONANDO REPOSITORIO AO LINUX (Freecad)"
- 	sudo add-apt-repository ppa:freecad-maintainers/freecad-daily -y
+	#Sublime
+echo "ADICIONANDO REPOSITORIO AO LINUX (Sublime)"
+ 	sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 	
 	#LibreOffice
-	echo "ADICIONANDO REPOSITORIO AO LINUX (LibreOffice)"
+echo "ADICIONANDO REPOSITORIO AO LINUX (LibreOffice)"
 	sudo add-apt-repository -y ppa:libreoffice/ppa
 	
 	
@@ -129,18 +150,28 @@ echo "INSTALANDO LibreOffice"
 	sudo apt-get install kicad -y
 	clear
 
-	#FreeCAD
-	echo "INSTALANDO FREECAD"
-  	sudo apt-get install freecad-daily freecad-daily-doc -y
+	#Sublime
+	echo "INSTALANDO Sublime"
+  	sudo apt-get install sublime-text-installer
 	clear
 
+	#RepetierHost
+	echo "BAIXANDO Repetier Host"
+	wget -c http://download.repetier.com/files/host/linux/repetierHostLinux_1_6_2.tgz
+	clear
+	echo "Descompactando Arquivo e Instalando"
+	tar -xzf repetierHostLinux_1_6_2.tgz
+	cd RepetierHost
+	bash configureFirst.sh
+	clear
+	
 	#Inkscape
 	echo "INSTALANDO Inkscape"
 	sudo apt-get install inkscape -y
 	clear
 	
 	echo "INSTALANDO LibreOffice"
-	sudo apt-get install libreoffice libreoffice-style-breeze
+	sudo apt-get install libreoffice libreoffice-style-breeze -y
 	clear;;
 
     0)
